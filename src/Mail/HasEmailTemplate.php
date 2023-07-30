5e8ec0b90bbef50cd721be9721a88594e4bae62c
Alessandro Pietrantonio <alessandro.pietrantonio@u2y.it>
2023-07-30 21:43:19 +0200

    feat: :sparkles: devs can now send email with subject with variables

--- src/Mail/HasEmailTemplate.php
+++ src/Mail/HasEmailTemplate.php
@@ -2,7 +2,6 @@
 
 namespace Pietrantonio\NovaMailManager\Mail;
 
-use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Pietrantonio\NovaMailManager\Models\EmailTemplate;
@@ -15,7 +14,7 @@ trait HasEmailTemplate
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: $this->emailTemplate->subject,
+            subject: $this->emailTemplate->getFormattedSubject(),
         );
     }
 
